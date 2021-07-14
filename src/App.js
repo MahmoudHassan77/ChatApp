9c669d3afccd227a56c161e965d882bc2a83e1d0
MahmoudHassanG2K <82512042+MahmoudHassanG2K@users.noreply.github.com>
2021-07-14 11:06:14 +0200

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     localStorage.setItem('user', user);
     try{
       const connection = new HubConnectionBuilder()
-      .withUrl("https://localhost:9090/chat")
+      .withUrl("http://trezeguit-001-site1.btempurl.com/chat")
       .configureLogging(LogLevel.Information)
       .build();
 
